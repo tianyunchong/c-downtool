9821a3c5215838b3b481e944dc57edf166872746
tianyunchong <854024109@qq.com>
2016-12-08 08:11:05 +0800

    初始化代码

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: zyh
+ *
+ * Created on 2016年12月8日, 上午7:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "test/test.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    testprint();
+    return (EXIT_SUCCESS);
+}
+
---
+++ test/test.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   test.h
+ * Author: zyh
+ *
+ * Created on 2016年12月8日, 上午7:59
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void testprint();
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TEST_H */
+
